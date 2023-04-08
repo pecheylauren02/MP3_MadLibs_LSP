7d8e234056a9214ec621a3cb7bb592b038fa4600
pecheylauren02 <pechey.lauren02@gmail.com>
2023-04-08 18:58:02 +0000

    Test story two template and inputs through test.py file

--- run.py
+++ run.py
@@ -97,12 +97,22 @@ any_number = input("Enter a random number: ")
 
 # Story Template Two of Eight
 
-story_two = f"""Once upon a time, in a land far, far away, there was a {adjective_one} {noun_one} who loved to {verb_one}.
-One day, while {verb_two + 'ing'} in the {place_one}, the {adjective_one} {noun_one} stumbled upon a {adjective_two} {noun_two} who was {verb_three + 'ing'} with a {adjective_three} {noun_three}.
-"Hey there, {exclamation}!" exclaimed the {adjective_one} {noun_one}. "Would you like to join us in our {adjective_four} {verb_one + 'ing'}?"
-Without hesitation, the {adjective_two} {noun_two} agreed and soon found themselves {verb_one + 'ing'} with the {adjective_two} {noun_two} and their {adjective_three} {noun_three}. They {verb_four} and {verb_four}, and even did a little {verb_five} together.
-But then, out of nowhere, a {adjective_four} {noun_four} appeared and started throwing things {adverb_one} at them! The {adjective_one} {noun_one} and the {adjective_two} {noun_two} were afraid, but to their surprise, the {adjective_three} {noun_three} stood tall and {verb_seven} the {adjective_four} {noun_four}.
-From {random_date}, the {adjective_one} {noun_one} made a promise to themselves to always be open to new experiences and never to be afraid of a little {verb_one}. And who knows? Maybe they'll even meet another {any_number} {adjective_four} {noun_four} along the way.
+story_two = f"""Once upon a time, in a land far, far away, there was a {adjective_one} {noun_one} who 
+loved to {verb_one}. One day, while {verb_two + 'ing'} in the {place_one}, the {adjective_one} {noun_one} 
+stumbled upon a {adjective_two} {noun_two} who was {verb_three + 'ing'} with a {adjective_three} {noun_three}.
+"Hey there, {exclamation}!" exclaimed the {adjective_one} {noun_one}. "Would you like to join us in our 
+{adjective_four} {verb_one + 'ing'}?"
+
+Without hesitation, the {adjective_two} {noun_two} agreed and soon found themselves {verb_one + 'ing'} 
+with the {adjective_two} {noun_two} and their {adjective_three} {noun_three}. They {verb_four} and {verb_four}, 
+and even did a little {verb_five} together.
+
+But then, out of nowhere, a {adjective_four} {noun_four} appeared and started throwing things {adverb_one} at them! 
+The {adjective_one} {noun_one} and the {adjective_two} {noun_two} were afraid, but to their surprise, the 
+{adjective_three} {noun_three} stood tall and {verb_six} the {adjective_four} {noun_four}.
+From {random_date}, the {adjective_one} {noun_one} made a promise to themselves to always be open to new experiences 
+and never to be afraid of a little {verb_one}. And who knows? Maybe they'll even meet another {any_number} 
+{adjective_four} {noun_four} along the way.
 The end."""
 
 print(story_two)
