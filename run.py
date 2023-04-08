6e92732414ba808d4b782be597bd3450b2e607ac
pecheylauren02 <pechey.lauren02@gmail.com>
2023-04-08 17:50:11 +0000

    Add inputs for story one and template for story titles

--- run.py
+++ run.py
@@ -1,12 +1,42 @@
 # Welcome message
+
+
 # Instructions
 
 # Mad Libs One of Eight
-# Story Template One of Eight
-# User inputs One of Eight
 
+# User inputs One of Eight: Status: DONE ALL 20
+adjective_one = input("Enter an adjective: ")
+feeling_one = input("Enter a feeling: ")
+noun_one = input("Enter a noun: ")
+place_one = input("Enter a place: ")
+adjective_two = input("Enter another adjective: ")
+animal_one = input("Enter a type of animal: ")
+noun_two = input("Enter another noun: ")
+adjective_three = input("Enter another adjective: ")
+verb_one = input("Enter a verb ending in -ed: ")
+plural_noun_one = input("Enter a plural noun: ")
+adjective_four = input("Enter another adjective: ")
+adverb_one = input("Enter an adverb: ")
+feeling_two = input("Enter another feeling: ")
+noun_three = input("Enter another noun: ")
+feeling_three = input("Enter another feeling: ")
+noun_four = input("Enter another noun: ")
+number_one = input("Enter a random number: ")
+adjective_five: input("Enter another adjective: ")
+feeling_four = input("Enter another feeling: ")
+verb_two = input("Enter a verb in the present tense: ")
 
+# Story Titles
 
+story_one = "A Peculiar Adventure"
+story_one = "A Peculiar Adventure"
+story_one = "A Peculiar Adventure"
+story_one = "A Peculiar Adventure"
+story_one = "A Peculiar Adventure"
+story_one = "A Peculiar Adventure"
+story_one = "A Peculiar Adventure"
+story_one = "A Peculiar Adventure"
 
 # Mad Libs Two of Eight
 # Story Template One of Eight
