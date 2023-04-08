23c7271eb83557e50ffe0e05ebfd8028cabf8da3
pecheylauren02 <pechey.lauren02@gmail.com>
2023-04-08 18:21:45 +0000

    Convert story one template to multiline string

--- run.py
+++ run.py
@@ -42,8 +42,8 @@ adjective_four = input("Enter another adjective: ")
 adverb_one = input("Enter an adverb: ")
 feeling_two = input("Enter another feeling: ")
 noun_three = input("Enter another noun: ")
-feeling_three = input("Enter another feeling: ")
 noun_four = input("Enter another noun: ")
+feeling_three = input("Enter another feeling: ")
 number_one = input("Enter a random number: ")
 adjective_five: input("Enter another adjective: ")
 feeling_four = input("Enter another feeling: ")
@@ -51,8 +51,29 @@ verb_two = input("Enter a verb in the present tense: ")
 
 # Story Template One of Eight
 
-story_one = f"One {adjective_one} day, I woke up feeling {feeling_one} and decided to go on a crazy adventure. I grabbed my trusty {noun_one} and set off into the {place_one} wilderness. As I walked, I came across a {adjective_two} {animal_one} who asked me for help. 'I've lost my {noun_two}!' they exclaimed. Being the {adjective_three} person I am, I offered to help. We {verb_one_past_tense} high and low, through dense forests and mysterious {plural_noun_one}, until finally we found the missing {noun_two}. The {noun_two} was so {adjective_four} that we couldn't resist taking a selfie {adverb_one} with it. Feeling {feeling_two}, I said goodbye to my new friend, {animal_one}, and continued on my wild adventure. Soon, I stumbled upon a giant {noun_three} who challenged me to a {noun_four} contest. I was feeling {feeling_three}, so I accepted. We went back and forth, with each of us throwing {number_one} punches at each other back and forth. In the end, I emerged victorious, much to the {adjective_five} disappointment of my opponent. Exhausted but {feeling_four}, I returned home and went straight to bed. As I drifted off to {verb_two}, I couldn't help but think that it had been a truly remarkable day. So if you're feeling {feeling_one} and in need of an adventure, grab your {noun_one} and set off into the {adjective_four} unknown. Who knows what kind of {adjective_three} and {adjective_two} things you might find along the way!"
+story_one = f"""One {adjective_one} day, I woke up feeling {feeling_one}
+and decided to go on a crazy adventure. I grabbed my trusty {noun_one} and
+set off into the {place_one} wilderness. As I walked, I came across a {adjective_two}
+{animal_one} who asked me for help. 'I've lost my {noun_two}!' they exclaimed. Being
+the {adjective_three} person I am, I offered to help. We {verb_one} high and
+low, through dense forests and mysterious {plural_noun_one}, until finally we found the
+missing {noun_two}. The {noun_two} was so {adjective_four} that we couldn't resist taking
+a selfie {adverb_one} with it. Feeling {feeling_two}, I said goodbye to my new friend, {animal_one},
+and continued on my wild adventure.
+
+Soon, I stumbled upon a giant {noun_three} who challenged me
+to a {noun_four} contest. I was feeling {feeling_three}, so I accepted. We went back and forth,
+with each of us throwing {number_one} punches at each other back and forth. In the end, I emerged
+victorious, much to the {adjective_five} disappointment of my opponent. Exhausted but {feeling_four},
+I returned home and went straight to bed.
+
+As I drifted off to {verb_two}, I couldn't help but think
+that it had been a truly remarkable day. So if you're feeling {feeling_one} and in need of an adventure,
+grab your {noun_one} and set off into the {adjective_four} unknown. Who knows what kind of
+{adjective_three} and {adjective_two} things you might find along the way!"""
+
 print(story_one)
+
 # User inputs One of Eight
 
 # Mad Libs One of Eight
