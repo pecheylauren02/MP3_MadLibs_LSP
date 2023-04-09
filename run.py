dde28dc82809a1187de4a0a58b24c86299bed047
pecheylauren02 <pechey.lauren02@gmail.com>
2023-04-09 12:13:35 +0000

    Correctly match user inputs to f string blanks

--- run.py
+++ run.py
@@ -134,7 +134,7 @@ elif selected_story_title == "A Day at the Office":
     noun_two = input("Enter another noun: ")
     adjective_one = input("Enter an adjective: ")
     feeling_two = input("Enter a feeling: ")
-    verb_one = input("Enter a verb: ")
+    verb_one = input("Enter a verb ending in -ing: ")
     noun_three = input("Enter a noun: ")
     verb_two = input("Enter another verb: ")
     any_number = input("Enter a random number: ")
@@ -149,23 +149,23 @@ elif selected_story_title == "A Day at the Office":
     noun_three = input("Enter another noun: ")
     clothing_plural = input("Enter an item of clothing (plural): ")
 
-    story_three = f'''It was just another {1. feeling_one} day in the office in {2. city_one} when I arrived at my 
-    large {3. noun_one}. I sat down at my {3. noun_one} and opened up my {4. noun_two} to start my {5. adjective_one} work. 
-    But before I could even begin, my {6. feeling_two} coworker walked in and started {7. verb_one + 'ing'} loudly 
-    about their {8. noun_three} problems. I tried to tune them out, but their voice was so annoying that I couldn't {9. verb_two}.
+    story_three = f'''It was just another {feeling_one} day in the office in {city_one} when I arrived at my 
+    large {noun_one}. I sat down at my {noun_one} and opened up my {noun_two} to start my {adjective_one} work. 
+    But before I could even begin, my {feeling_two} coworker walked in and started {verb_one} loudly 
+    about their {noun_three} problems. I tried to tune them out, but their voice was so annoying that I couldn't {9. verb_two}.
     
-    After what felt like {10. Any number} hours of their constant {7. verb_one + 'ing'}, I decided to take a {11. adjective_two} 
-    break and visit the office in {12. city_two}.As I walked {13. adverb_one} down the hallway, I noticed a {14. adjective_three} 
-    smell coming from the break room. I cautiously peeked inside, and to my horror, I saw that someone had left their {15. noun_four} 
+    After what felt like {any_number} hours of their constant {verb_one}, I decided to take a {adjective_two} 
+    break and visit the office in {city_two}.As I walked {adverb_one} down the hallway, I noticed a {adjective_three} 
+    smell coming from the break room. I cautiously peeked inside, and to my horror, I saw that someone had left their {noun_four} 
     in the microwave for way too long.
     
-    The {15. noun_four} was now a {16. adjective_four} mess, and the entire room smelled like burnt {17. plural_noun_four}.
-    Feeling {18. feeling_three}, I quickly made my way back to my {3. noun_one}, only to find that my {4. noun_two} had 
-    mysteriously disappeared! After a few minutes of searching {19. adverb_one}, I finally found it hiding under a pile of 
-    dirty {20. Piece of clothing(plural)}.
+    The {noun_four} was now a {adjective_four} mess, and the entire room smelled like burnt {plural_noun_four}.
+    Feeling {18. feeling_three}, I quickly made my way back to my {noun_one}, only to find that my {noun_two} had 
+    mysteriously disappeared! After a few minutes of searching {adverb_one}, I finally found it hiding under a pile of 
+    dirty {clothing_plural}.
     
-    So if you're ever feeling bored at work, just remember to keep your eyes and ears open for any weird moments that might come your way.
-    You never know what kind of {15. noun_three} might be waiting just around the corner in {2. city_one}!'''
+    So if you're ever feeling bored at work, just remember to keep your eyes and ears open for any weird moments that might 
+    come your way. You never know what kind of {noun_three} might be waiting just around the corner in {city_one}!'''
 
 # User inputs One of Eight
 
