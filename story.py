2eb815be9762d85167085d5c978efe35c8c2f3dd
pecheylauren02 <pechey.lauren02@gmail.com>
2023-04-14 16:55:14 +0000

    Create story.py file for stories dictionary

---
+++ story.py
@@ -0,0 +1,9 @@
+STORIES = [
+   {
+      'title': "A Peculiar Adventure",
+      'words': ['an adjective', 'a feeling', 'a noun', 'a place', 'another adjective', 'an animal', 'a noun', 
+        'another adjective', 'a verb ending in -ed', 'a plural noun', 'another adjective', 'an adverb', 'a feeling', 
+        'another noun', 'another noun', 'another feeling', 'a number', 'another adjective', 'another feeling', 'a verb in the present tense'],
+        'text': f"""One {0} day, I woke up feeling {1}"""
+      },
+]
