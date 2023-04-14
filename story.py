cb38c941e3bac037532d94dbda24cda46e35b0f7
pecheylauren02 <pechey.lauren02@gmail.com>
2023-04-14 17:16:52 +0000

    Add and indent story one to dictionary

--- story.py
+++ story.py
@@ -1,9 +1,35 @@
 STORIES = [
    {
       'title': "A Peculiar Adventure",
-      'words': ['an adjective', 'a feeling', 'a noun', 'a place', 'another adjective', 'an animal', 'a noun', 
-        'another adjective', 'a verb ending in -ed', 'a plural noun', 'another adjective', 'an adverb', 'a feeling', 
-        'another noun', 'another noun', 'another feeling', 'a number', 'another adjective', 'another feeling', 'a verb in the present tense'],
-        'text': f"""One {0} day, I woke up feeling {1}"""
-      },
+      'words': ['an adjective', 'a feeling', 'a noun', 'a place',
+                'another adjective', 'an animal', 'a noun',
+                'another adjective', 'a verb ending in -ed',
+                'a plural noun', 'another adjective', 'an adverb',
+                'a feeling', 'another noun', 'another noun',
+                'another feeling', 'a number', 'another adjective',
+                'another feeling', 'a verb in the present tense'],
+        'text': f"""One {0} day, I woke up feeling {1} and decided to go
+                on a crazy adventure. I grabbed my trusty {2} and set off
+                into the {3} wilderness. As I walked, I came across a {4} {5}
+                who asked me for help. 'I've lost my {6}!' they exclaimed.\n
+                Being the {7} person I am, I offered to help. We {8} high and
+                low, through dense forests and mysterious {9}, until finally we
+                found the missing {6}. The {6} was so {10} that we couldn't
+                resist taking a selfie {11} with it. Feeling {12}, I said
+                goodbye to my new friend, {5}, and continued on my wild
+                adventure.\n
+                Soon, I stumbled upon a giant {13} who challenged me to
+                a {14} contest. I was feeling {15}, so I accepted. We
+                went back and forth, with each of us throwing {16}
+                punches at each other. In the end, I emerged victorious,
+                much to the {17} disappointment of my opponent.
+                Exhausted but {18}, I returned home and went straight
+                to bed.\n
+                As I drifted off to {19}, I couldn't help but think
+                that it had been a truly remarkable day. So if you're
+                feeling {1} and in need of an adventure, grab your {2}
+                and set off into the {10} unknown. Who knows
+                what kind of {7} and {4} things you might find
+                along the way!"""
+    },
 ]
