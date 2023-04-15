f29041bc2d1fde10089422922b07a628586fe239
pecheylauren02 <pechey.lauren02@gmail.com>
2023-04-15 10:48:23 +0000

    Fix indentation errors for story selection area

--- run.py
+++ run.py
@@ -65,17 +65,25 @@ def start_game():
 # Define the list of story titles
 
 def play_madlibs():
-    story_titles = ["A Peculiar Adventure", "A Strange Fairytale", "A Day at the Office", 
-                    "One Interesting Vacation", "A Scary Encounter", "A Day at the Beach", 
-                    "My First Day at School", "A Wild Party"]
+    story_titles = ["A Peculiar Adventure", 
+                    "A Strange Fairytale", 
+                    "A Day at the Office", 
+                    "One Interesting Vacation", 
+                    "A Scary Encounter",
+                    "A Day at the Beach", 
+                    "My First Day at School", 
+                    "A Wild Party"]
 
     # Print the list of story titles for the user to see
-    print("Please select a story by entering the corresponding number:")
+    print("""Please select a story by
+          entering the corresponding number:""")
     for i, title in enumerate(story_titles):
         print(f"{i+1}. {title}")
 
     # Ask the user to select a story
-    selected_story_index = int(input("Enter the number of the story you'd like to read: ")) - 1
+    selected_story_index = int(input("""Enter the number 
+                                    of the story you'd
+                                    like to read: """)) - 1
 
     # Retrieve the selected story title
     selected_story_title = story_titles[selected_story_index]
